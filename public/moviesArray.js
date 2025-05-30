2b437ec99e4d922b6d8213802edbad0e459eb403
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-05-30 15:14:28 +0200

    [UPD] Create layout components and add routes

---
+++ public/moviesArray.js
@@ -0,0 +1,94 @@
+const film = [
+    {
+        id: 1,
+        title: "Inception",
+        director: "Christopher Nolan",
+        genre: "Fantascienza",
+        release_year: 2010,
+        abstract: "Un ladro che ruba segreti attraverso la condivisione di sogni viene incaricato di piantare un'idea nella mente di un CEO.",
+        image: "https://image.tmdb.org/t/p/w500/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg"
+    },
+    {
+        id: 2,
+        title: "Il Padrino",
+        director: "Francis Ford Coppola",
+        genre: "Gangster",
+        release_year: 1972,
+        abstract: "Il patriarca di una dinastia criminale passa il controllo del suo impero al figlio riluttante.",
+        image: "https://image.tmdb.org/t/p/w500/r4gnMXoY1efvaolNDjn3nj4046S.jpg"
+    },
+    {
+        id: 3,
+        title: "La Vita è Bella",
+        director: "Roberto Benigni",
+        genre: "Drammatico",
+        release_year: 1997,
+        abstract: "Un padre ebreo usa la fantasia per proteggere suo figlio dagli orrori di un campo di concentramento.",
+        image: "https://image.tmdb.org/t/p/w500/mfnkSeeVOBVheuyn2lo4tfmOPQb.jpg"
+    },
+    {
+        id: 4,
+        title: "C'era una volta in America",
+        director: "Sergio Leone",
+        genre: "Gangster",
+        release_year: 1984,
+        abstract: "Un ex gangster torna a New York dopo decenni, rivivendo ricordi di amicizia, crimine e tradimento.",
+        image: "https://image.tmdb.org/t/p/w500/i0enkzsL5dPeneWnjl1fCWm6L7k.jpg"
+    },
+    {
+        id: 5,
+        title: "Nuovo Cinema Paradiso",
+        director: "Giuseppe Tornatore",
+        genre: "Drammatico",
+        release_year: 1988,
+        abstract: "Un regista di successo torna in Sicilia e ricorda la sua infanzia e l'amicizia con un proiezionista.",
+        image: "https://image.tmdb.org/t/p/w500/1kUbi4z0QqdzvK8RqdEaMlZfeQz.jpg"
+    },
+    {
+        id: 6,
+        title: "Il Gladiatore",
+        director: "Ridley Scott",
+        genre: "Storico/Azione",
+        release_year: 2000,
+        abstract: "Un generale romano diventa gladiatore per vendicarsi dell'imperatore che ha ucciso la sua famiglia.",
+        image: "https://image.tmdb.org/t/p/w500/6WBIzCgmDCYrqh64yDREGeDk9d3.jpg"
+    },
+    {
+        id: 7,
+        title: "Interstellar",
+        director: "Christopher Nolan",
+        genre: "Fantascienza",
+        release_year: 2014,
+        abstract: "Un gruppo di esploratori viaggia attraverso un wormhole per trovare un nuovo pianeta abitabile.",
+        image: "https://image.tmdb.org/t/p/w500/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg"
+    },
+    {
+        id: 8,
+        title: "La La Land",
+        director: "Damien Chazelle",
+        genre: "Musical",
+        release_year: 2016,
+        abstract: "Un pianista jazz e un'aspirante attrice si innamorano a Los Angeles, tra sogni e realtà.",
+        image: "https://image.tmdb.org/t/p/w500/uDO8zWDhfWwoFdKS4fzkUJt0Rf0.jpg"
+    },
+    {
+        id: 9,
+        title: "Parasite",
+        director: "Bong Joon-ho",
+        genre: "Thriller",
+        release_year: 2019,
+        abstract: "Una povera famiglia coreana si infiltra nella vita di una ricca famiglia, con conseguenze drammatiche.",
+        image: "https://image.tmdb.org/t/p/w500/7IiTTgloJzvGI1TAYymCfbfl3vT.jpg"
+    },
+    {
+        id: 10,
+        title: "Django Unchained",
+        director: "Quentin Tarantino",
+        genre: "Western",
+        release_year: 2012,
+        abstract: "Uno schiavo liberato diventa un cacciatore di taglie per ritrovare sua moglie.",
+        image: "https://image.tmdb.org/t/p/w500/5WJnxuw41sddupf8cwOxYftuvJG.jpg"
+    }
+];
+
+export default movies;
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import DefaultLayout from '../layouts/DefaultLayout'
+import HomePage from '../components/HomePage'
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route Component={DefaultLayout}>
+          <Route index Component={HomePage} />
+        </Route>
+      </Routes>
+    </BrowserRouter >
   )
 }
 
